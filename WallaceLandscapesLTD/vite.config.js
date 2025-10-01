51168f2caab3e4cc49b2519df5ff113b88f5fb1a
Gabe Woolaver <woolaverg@gmail.com>
2025-10-01 20:09:38 -0300

    Updates

--- WallaceLandscapesLTD/vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  // IMPORTANT: must match your GitHub repo name exactly
-  base: "/WallaceLanscapesLtd/",
-});
