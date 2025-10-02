57826a7e2a40ad15eabe4d9a2191eb6e2e715b90
Gabe Woolaver <woolaverg@gmail.com>
2025-10-02 12:58:17 -0300

    fixing back vite.config

--- WallaceLandscapesLTD/vite.config.js
+++ WallaceLandscapesLTD/vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // IMPORTANT: must match your GitHub repo name exactly
-  base: '/',
+  base: '/WallaceLandscapesLtd/',
 });
